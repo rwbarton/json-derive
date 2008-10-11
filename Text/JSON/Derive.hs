f8463d423f989162cec82142b2822c7ac0747416
Reid Barton <rwbarton@alum.mit.edu>
2008-10-10 22:44:14 -0400

    Ugly but working readJSON code

--- Text/JSON/Derive.hs
+++ Text/JSON/Derive.hs
@@ -1,5 +1,6 @@
 module Text.JSON.Derive (makeJSON) where
 
+import Control.Arrow (first)
 import Language.Haskell.TH.All
 
 makeJSON :: Derivation
@@ -8,8 +9,11 @@ makeJSON = derivation derive "JSON"
 stringLit :: Valcon a => String -> a
 stringLit = raw_lit . StringL
 
+where' :: Clause -> Dec -> Clause
+where' (Clause ps body decs) d = Clause ps body (d:decs)
+
 derive :: DataDef -> [Dec]
-derive dat = simple_instance "JSON" dat [funN "showJSON" sbody]
+derive dat = simple_instance "JSON" dat [funN "showJSON" sbody, funN "readJSON" rbody]
   where sbody = [ sclause [ctp ctor 'x'] (show_case ctor) | ctor <- ctors ]
         show_case ctor@NormalC {} = show_normal ctor
         show_case ctor@InfixC {}  = show_normal ctor
@@ -23,6 +27,29 @@ derive dat = simple_instance "JSON" dat [funN "showJSON" sbody]
           tag . l1 "showJSON" . l1 "toJSObject" . lst $ zipWith (\x y -> tup [stringLit x, l1 "showJSON" y]) (ctorFields ctor) (ctv ctor 'x')
             where tag | multipleCtors = \x -> l1 "showJSON" $ lst [l1 "showJSON" . stringLit $ ctorName ctor, x]
                       | otherwise     = id
+
+        rbody | multipleCtors = [(sclause [l1 "JSArray" $ l2 ":" (vr "tag") (vr "v")] $ case' (tup [l1 "readJSON" $ vr "tag", vr "v"]) $
+                                  [ first (tup . (l1 "Ok" (stringLit (ctorName ctor)) :) . (:[])) $ read_clause ctor | ctor <- ctors ] ++ [failure_case])
+                                 `where'` lookupField_def,
+                                 failure_clause
+                                ]
+              | otherwise     = [(sclause [vr "v"] $ case' (vr "v") (map read_clause ctors ++ [failure_case])) `where'` lookupField_def]
+        read_clause ctor@NormalC {} = read_normal ctor
+        read_clause ctor@InfixC {}  = read_normal ctor
+        read_clause ctor@RecC {}    = read_record ctor
+        read_clause      ForallC {} = error "read_clause: can't handle existential constructors"
+        read_normal ctor =
+          (munge $ lst $ ctv ctor 'x', liftmk (ctc ctor) (map (l1 "readJSON") (ctv ctor 'x')))
+            where munge | multipleCtors = id
+                        | otherwise     = l1 "JSArray"
+        read_record ctor =
+          (munge . l1 "JSObject" $ vr "obj", LetE [ValD (vr "alist") (NormalB (l1 "fromJSObject" $ vr "obj")) []] $ liftmk (ctc ctor) [ (l2 "lookupField" (stringLit name) (vr "alist")) >>=: l0 "readJSON" | name <- ctorFields ctor ])
+            where munge | multipleCtors = \x -> lst [x]
+                        | otherwise     = id
+        err = (l1 "Error" $ stringLit ("Unable to read " ++ dataName dat))
+        failure_case = (WildP, err)
+        failure_clause = sclause [fst failure_case] (snd failure_case)
+        lookupField_def = funN "lookupField" $ [sclause [vr "s", vr "a"] $ lK "maybe" [err, l0 "Ok", (l2 "lookup" (vr "s") (vr "a"))]]
 
         ctors = dataCtors dat
         multipleCtors = length ctors > 1
