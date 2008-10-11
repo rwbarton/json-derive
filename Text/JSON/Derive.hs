ff1e90134b4b1da379fbb4eab9949a192107b03c
Reid Barton <rwbarton@alum.mit.edu>
2008-10-10 22:43:54 -0400

    Minor cleanup

--- Text/JSON/Derive.hs
+++ Text/JSON/Derive.hs
@@ -5,20 +5,23 @@ import Language.Haskell.TH.All
 makeJSON :: Derivation
 makeJSON = derivation derive "JSON"
 
+stringLit :: Valcon a => String -> a
+stringLit = raw_lit . StringL
+
 derive :: DataDef -> [Dec]
 derive dat = simple_instance "JSON" dat [funN "showJSON" sbody]
   where sbody = [ sclause [ctp ctor 'x'] (show_case ctor) | ctor <- ctors ]
         show_case ctor@NormalC {} = show_normal ctor
         show_case ctor@InfixC {}  = show_normal ctor
         show_case ctor@RecC {}    = show_record ctor
-        show_case      ForallC {} = error "show_case (ForallC {})" -- It's currently impossible to reify existential constructors anyways.
+        show_case      ForallC {} = error "show_case: can't handle existential constructors"
         show_normal ctor =      -- Array of fields, possibly with constructor tag at beginning
           l1 "JSArray" . lst . map (l1 "showJSON") . tag $ ctv ctor 'x'
-            where tag | multipleCtors = (lit (ctorName ctor) :)
+            where tag | multipleCtors = (stringLit (ctorName ctor) :)
                       | otherwise     = id
         show_record ctor =      -- Either an object or a [constructor tag, object] pair (array)
-          l1 "showJSON" . tag . l1 "showJSON" . l1 "toJSObject" . lst $ zipWith (\x y -> tup [lit x, l1 "showJSON" y]) (ctorFields ctor) (ctv ctor 'x')
-            where tag | multipleCtors = \x -> lst [l1 "showJSON" . lit $ ctorName ctor, x]
+          tag . l1 "showJSON" . l1 "toJSObject" . lst $ zipWith (\x y -> tup [stringLit x, l1 "showJSON" y]) (ctorFields ctor) (ctv ctor 'x')
+            where tag | multipleCtors = \x -> l1 "showJSON" $ lst [l1 "showJSON" . stringLit $ ctorName ctor, x]
                       | otherwise     = id
 
         ctors = dataCtors dat
