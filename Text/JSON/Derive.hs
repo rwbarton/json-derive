6f52baf488ee056275e48eddf6a4f16eb0c35b55
Reid Barton <rwbarton@alum.mit.edu>
2008-10-12 17:37:23 -0400

    readJSON code cleanup

--- Text/JSON/Derive.hs
+++ Text/JSON/Derive.hs
@@ -6,12 +6,6 @@ import Language.Haskell.TH.All
 makeJSON :: Derivation
 makeJSON = derivation derive "JSON"
 
-stringLit :: Valcon a => String -> a
-stringLit = raw_lit . StringL
-
-where' :: Clause -> Dec -> Clause
-where' (Clause ps body decs) d = Clause ps body (d:decs)
-
 derive :: DataDef -> [Dec]
 derive dat = simple_instance "JSON" dat [funN "showJSON" sbody, funN "readJSON" rbody]
   where sbody = [ sclause [ctp ctor 'x'] (show_case ctor) | ctor <- ctors ]
@@ -28,28 +22,44 @@ derive dat = simple_instance "JSON" dat [funN "showJSON" sbody, funN "readJSON" 
             where tag | multipleCtors = \x -> l1 "showJSON" $ lst [l1 "showJSON" . stringLit $ ctorName ctor, x]
                       | otherwise     = id
 
-        rbody | multipleCtors = [(sclause [l1 "JSArray" $ l2 ":" (vr "tag") (vr "v")] $ case' (tup [l1 "readJSON" $ vr "tag", vr "v"]) $
-                                  [ first (tup . (l1 "Ok" (stringLit (ctorName ctor)) :) . (:[])) $ read_clause ctor | ctor <- ctors ] ++ [failure_case])
+        -- With multiple constructors, we read an array starting with a constructor tag
+        rbody | multipleCtors = [(sclause [l1 "JSArray" $ l2 ":" (vr "tag") (vr "v")] $ -- v is the list of fields or a singleton list containing the object
+                                  case' (tup [l1 "readJSON" $ vr "tag", vr "v"]) $
+                                  [ add_tag_to_pattern (ctorName ctor) $ read_clause ctor | ctor <- ctors ] ++ [failure_case])
                                  `where'` lookupField_def,
                                  failure_clause
                                 ]
-              | otherwise     = [(sclause [vr "v"] $ case' (vr "v") (map read_clause ctors ++ [failure_case])) `where'` lookupField_def]
+              | otherwise     = [(sclause [vr "v"] $ case' (vr "v") (map read_clause ctors ++ [failure_case])) `where'` lookupField_def] -- v is simply the value
+        add_tag_to_pattern s (pat, body) = (tup [l1 "Ok" (stringLit s), pat] , body)
         read_clause ctor@NormalC {} = read_normal ctor
         read_clause ctor@InfixC {}  = read_normal ctor
         read_clause ctor@RecC {}    = read_record ctor
         read_clause      ForallC {} = error "read_clause: can't handle existential constructors"
         read_normal ctor =
           (munge $ lst $ ctv ctor 'x', liftmk (ctc ctor) (map (l1 "readJSON") (ctv ctor 'x')))
-            where munge | multipleCtors = id
+            where munge | multipleCtors = id -- With multiple constructors, the JSArray has already been pulled off
                         | otherwise     = l1 "JSArray"
         read_record ctor =
-          (munge . l1 "JSObject" $ vr "obj", LetE [ValD (vr "alist") (NormalB (l1 "fromJSObject" $ vr "obj")) []] $ liftmk (ctc ctor) [ (l2 "lookupField" (stringLit name) (vr "alist")) >>=: l0 "readJSON" | name <- ctorFields ctor ])
-            where munge | multipleCtors = \x -> lst [x]
+          (munge . l1 "JSObject" $ vr "obj",
+           LetE [ValD (vr "alist") (NormalB (l1 "fromJSObject" $ vr "obj")) []] $ -- alist is the list of associations
+           liftmk (ctc ctor) [ (l2 "lookupField" (stringLit name) (vr "alist")) >>=: l0 "readJSON" | name <- ctorFields ctor ])
+            where munge | multipleCtors = \x -> lst [x] -- With multiple constructors, we receive a one-element list
                         | otherwise     = id
-        err = (l1 "Error" $ stringLit ("Unable to read " ++ dataName dat))
+        err = l1 "Error" $ stringLit ("Unable to read " ++ dataName dat)
         failure_case = (WildP, err)
-        failure_clause = sclause [fst failure_case] (snd failure_case)
+        failure_clause = sclause [fst failure_case] err
         lookupField_def = funN "lookupField" $ [sclause [vr "s", vr "a"] $ lK "maybe" [err, l0 "Ok", (l2 "lookup" (vr "s") (vr "a"))]]
 
         ctors = dataCtors dat
         multipleCtors = length ctors > 1
+
+-- | Produce a string literal expression or pattern.
+-- `lit` produces a list of character literals,
+-- which is harder to read when debugging.
+stringLit :: Valcon a => String -> a
+stringLit = raw_lit . StringL
+
+-- | Add a `where` clause to a declaration.
+where' :: Clause -> Dec -> Clause
+where' (Clause ps body decs) d = Clause ps body (d:decs)
+
